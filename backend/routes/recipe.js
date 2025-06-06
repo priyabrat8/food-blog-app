681b2abc22f56cc008495a8daae6cd07fdc3b56c
Priyabrat Swain <priyabratswain888@gmail.com>
2025-06-06 23:58:47 +0530

    Add routes for recipes.

---
+++ backend/routes/recipe.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { getRecipes,getRecipe,addRecipe,updateRecipe, deleteRecipe } = require('../controller/recipe');
+const router = express.Router();
+
+router.get('/', getRecipes); // Get all recipes
+router.get('/:id', getRecipe); // Get a specific recipe by ID
+router.post('/', addRecipe); // Add a new recipe
+router.put('/:id', updateRecipe); // Update a specific recipe by ID
+router.delete('/:id', deleteRecipe); // Delete a specific recipe by ID
+
+module.exports = router;
